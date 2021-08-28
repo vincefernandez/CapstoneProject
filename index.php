4272099cb81be91cf67a0d4978310b9af2631acc
Vincent Fernandez <vincentfernandez727@gmail.com>
2021-08-28 23:50:09 +0800

    finals

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once('app/classes.php');
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Quezon City University SAMPLE</title>
+  <title>Quezon City University</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
